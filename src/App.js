659495f54ce75a2ac7b19ca33c970fa0b12c4170
Mabunay <mabunaygo@MNLPREPDSN2WSA.local>
2021-02-13 22:38:33 +0800

    for Deployment

--- src/App.js
+++ src/App.js
@@ -3,17 +3,28 @@ import React from "react";
 import Navbar from "./components/Navbar";
 import CartContainer from "./components/CartContainer";
 // items
-import cartItems from "./cart-items";
+
 // redux stuff
+
+import { createStore } from 'redux';
+import reducer from './reducer';
+// react-redux - Provider - wraps app, connect - used in components
+import { Provider } from 'react-redux';
+
+// store
+const store = createStore(
+  reducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 function App() {
   // cart setup
 
   return (
-    <main>
+    <Provider store={store}>
       <Navbar />
-      <CartContainer cart={cartItems} />
-    </main>
+      <CartContainer />
+    </Provider>
   );
 }
 
